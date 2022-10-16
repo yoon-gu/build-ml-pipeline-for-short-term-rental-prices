a4724a1ab02aca72f938b5c7d9d2b62408671276
Yoon-gu Hwang <yz0624@gmail.com>
2022-10-16 12:02:00 +0900

    step 2

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,7 +5,8 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 import argparse
 import logging
 import wandb
-
+import pandas as pd
+import os
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -18,19 +19,28 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
     ######################
     # YOUR CODE HERE     #
     ######################
     logger.info("Dropping outliers")
+    df = pd.read_csv(artifact_local_path)
     # Drop outliers
-    min_price = 10
-    max_price = 350
-    idx = df['price'].between(min_price, max_price)
+    idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    df.to_csv(args.output_artifact, index=False)
+
+    logger.info("Logging artifact")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(args.output_artifact)
 
 
 if __name__ == "__main__":
